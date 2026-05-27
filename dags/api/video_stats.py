f3337f05a3112f1d3cb2eabef659970059236e63
raivskal <79742785+raivskal@users.noreply.github.com>
2026-05-27 12:53:30 +0300

    Update for airflow dag

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,14 +1,20 @@
 import requests, json
 import os
-from dotenv import load_dotenv
+
 from datetime import date
 
-load_dotenv(dotenv_path="./.env")
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env") -- not needed anymore since we are using Airflow Variables to store the API key and channel handle
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+from airflow.decorators import task
+from airflow.models import Variable
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
 
     try:
@@ -36,6 +42,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def get_video_ids(playlist_id):
 
     video_ids = []
@@ -75,11 +82,7 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:    
         raise e    
     
-
-
-
-
-
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -129,7 +132,9 @@ def extract_video_data(video_ids):
 
     except requests.exceptions.RequestException as e:
         raise e
-    
+
+
+@task
 def save_to_json(extracted_data):
     os.makedirs("./data", exist_ok=True)
     file_path = f"./data/YT_data_{date.today()}.json"
---
+++ dags/main.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+import pendulum
+from datetime import timedelta, datetime
+from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
+
+#Define the local timezone
+local_tz = pendulum.timezone("Europe/Riga")
+
+#Default Args
+default_args = {
+    "owner": "dataengineers",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email":"hello@raivis.it",
+    # "retries": 1,
+    # "retry_delay": timedelta(minutes=5),
+    "max_active_runs": 1,
+    "dagrun_timeout": timedelta(minutes=60),
+    "start_date": datetime(2026, 5, 27, tzinfo=local_tz),
+    # "end_date": datetime(2024, 6, 30, tzinfo=local_tz)
+}
+
+with DAG(
+     dag_id="produce_json",
+     default_args=default_args,
+     description="A DAG to extract video stats from YouTube API and save it as JSON",
+     schedule="0 14 * * *", # Run daily at 14:00 (2 PM) local time
+     catchup=False
+) as dag:
+    
+    # Define the tasks
+    playlist_id = get_playlist_id()
+    video_ids = get_video_ids(playlist_id)
+    extracted_data = extract_video_data(video_ids)
+    save_to_json_task = save_to_json(extracted_data)
+
+    # Define dependencies
+    playlist_id >> video_ids >> extracted_data >> save_to_json_task
